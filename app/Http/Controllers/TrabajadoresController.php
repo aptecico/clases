8bbdd54ffcda1bf7fccb75c4c07abf918d97687b
aptecico <german.pinchao@gmail.com>
2025-03-12 10:08:52 -0500

    modulos de empleados y departamentos

---
+++ app/Http/Controllers/TrabajadoresController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Trabajadores;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TrabajadoresController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Trabajadores $trabajadores)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Trabajadores $trabajadores)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Trabajadores $trabajadores)
+    {
+        //
+    }
+}
